9595826991066f63dd954c648730b3e3575aa019
daniel-platform <daniel@platform.sh>
2019-03-30 17:36:08 -0400

    Improved startup script

--- start.sh
+++ start.sh
@@ -14,11 +14,20 @@ export MB_DB_USER=$(bin/json_env PLATFORM_RELATIONSHIPS metabasedb.metabasedb.us
 export MB_DB_PASS=$(bin/json_env PLATFORM_RELATIONSHIPS metabasedb.metabasedb.password)
 export MB_DB_HOST=$(bin/json_env PLATFORM_RELATIONSHIPS metabasedb.metabasedb.host)
 
+# Email
+export MB_EMAIL_SMTP_HOST=$PLATFORM_SMTP_HOST
+export MB_EMAIL_SMTP_PORT=25
+export MB_EMAIL_SMTP_USERNAME=""
+export MB_EMAIL_SMTP_PASSWORD=""
+
 # Grab memory limits
 export MEM_AVAILABLE=$(bin/jq .info.limits.memory /run/config.json)
 
 # Limit Heapsize
 export JAVA_TOOL_OPTIONS="-XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:MaxPermSize=${MEM_AVAILABLE}m -Xmx${MEM_AVAILABLE}m"
+
+# This ensures that the child process below gets stopped when Platform.sh kills this script.
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 #java -jar ${METABASE_HOME}/${METABASE_JAR} migrate release-locks
 java -jar ${METABASE_HOME}/${METABASE_JAR}
