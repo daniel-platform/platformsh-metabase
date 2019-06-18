b8ab81c82a6e9a5b2b231140ddeb7f67b0f505e6
daniel-platform <daniel@platform.sh>
2019-06-18 01:29:40 -0400

    Update to 0.32.9

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@
 
 # Desired version can be set by means of an enviromental variable
 if [ -z "$METABASE_VERSION" ]; then 
-	# Default to Metabase 0.32.1
-	METABASE_VERSION=0.32.1; 
+	# Default to Metabase 0.32.9
+	METABASE_VERSION=0.32.9; 
 fi
 
 METABASE_DOWNLOAD_URI="http://downloads.metabase.com/v${METABASE_VERSION}"
